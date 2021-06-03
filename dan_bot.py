6fd70a60369bdbb4e46831c0559c2caaafb04c92
Daniil Kerna <danilkak101@gmail.com>
2021-06-03 10:19:26 -0700

    dan bot 1.0

--- dan_bot.py
+++ dan_bot.py
@@ -1,44 +1,57 @@
 import config
 import scout_data as sd
-from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+from binance import Client
 from datetime import datetime
 import time
 import math
 
 client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
-VERSION = 1.00
+VERSION = 1.01
 
 
 POSITION = False
 COOLDOWN_SECONDS = 0
 PURCHASE_PRICE = 0
+UPTREND = False
 
 def truncate(f, n):
     return math.floor(f * 10 ** n) / 10 ** n
 
 def get_account_cash_balance():
     cash = client.get_asset_balance(asset='USD')['free']
-    return cash
+    return float(cash)
 
 def get_coin_balance(coin):
     balance = client.get_asset_balance(asset=coin)['free']
-    return balance
+    return float(balance)
 
 def buy_coin(coin):
     global POSITION
     global PURCHASE_PRICE
     global COOLDOWN_SECONDS
 
-    PURCHASE_PRICE = sd.get_coin_price(coin)
+    PURCHASE_PRICE = sd.get_latest_coin_price(coin)
     
     order = client.order_market_buy(
     symbol= coin + 'USD',
     quantity= truncate((get_account_cash_balance() / PURCHASE_PRICE), 5) - 0.1 )
     print(order)
 
-    POSITION = True
-    COOLDOWN_SECONDS = 10
+    try:
+        file_object = open('log.txt', 'a')
+        file_object.write("\n\n")
+        file_object.write('Purchase')
+        file_object.write("\n")
+        file_object.write(str(datetime.now()))
+        file_object.write("\n")
+        file_object.write(str(order))
+        file_object.write("\n")
+        file_object.close()
+
+    finally:
+        POSITION = True
+        COOLDOWN_SECONDS = 10
 
 
 def sell_coin(coin):
@@ -51,55 +64,108 @@ def sell_coin(coin):
     quantity= truncate(get_coin_balance(coin) , 5))    
     print(order)
 
-    POSITION = False
-    COOLDOWN_SECONDS = 10
-    PURCHASE_PRICE = 0
+    try:
+        file_object = open('log.txt', 'a')
+        file_object.write("\n\n")
+        file_object.write('Sell Order')
+        file_object.write("\n")
+        file_object.write(str(datetime.now()))
+        file_object.write("\n")
+        file_object.write(str(order))
+        file_object.close()
 
+    finally:
+        POSITION = False
+        COOLDOWN_SECONDS = 10
+        PURCHASE_PRICE = 0
+
+def set_uptrend(coin):
+    global UPTREND
+    old_uptrend = UPTREND
+    closing_price = sd.get_latest_closing_price(coin)
+    mov_avg = sd.get_coin_mov_avg(coin , config.TREND_MA)
+    # print(closing_price , mov_avg , UPTREND)
+    if closing_price == -1.0 or mov_avg == -1.0 :
+        return
+
+    if closing_price >= mov_avg:
+        UPTREND = True
+    else:
+        UPTREND = False
+
+    if old_uptrend != UPTREND:
+        try:
+            file_object = open('log.txt', 'a')
+            file_object.write("\n")
+            file_object.write('New Trend Detected. Uptrend is now:' + str(UPTREND) + "\n")
+            file_object.write(str(datetime.now()))
+            file_object.write("\n")
+            file_object.close()
+        finally:
+            print("New Trend Detected")
+            print("Uptrend is now: " + str(UPTREND))
+            print(datetime.now())
 
 def main():
     global POSITION
     global PURCHASE_PRICE
     global COOLDOWN_SECONDS
+    global UPTREND
 
     while True:
-        if COOLDOWN_SECONDS > 0:
-            time.sleep(COOLDOWN_SECONDS)
-            COOLDOWN_SECONDS = 0
-            
-        for coin in config.supported_coins:
-            current_price = sd.get_coin_price(coin)
-            print(current_price)
-            print(datetime.now())
+        try:
+            if COOLDOWN_SECONDS > 0:
+                time.sleep(COOLDOWN_SECONDS)
+                COOLDOWN_SECONDS = 0
+                
+            for coin in config.supported_coins:
+                current_price = sd.get_latest_coin_price(coin)
+                time.sleep(0.5)
+                closing_price = sd.get_latest_closing_price(coin)
+                if (current_price == -1.0 or closing_price == -1.0):
+                    continue
 
-            if not POSITION and sd.is_coin_oversold(coin):
-                # enter into a position
-                print("Buying Coin :" + coin)
-                print(datetime.now())
-                print("Price:" + str(current_price))
+                set_uptrend(coin)
 
-                buy_coin(coin)
-
-            if POSITION:
-                # potential profit
-                if sd.is_coin_overbought(coin):
-                    print("Exiting Position")
+                if not POSITION and sd.is_coin_oversold(coin , UPTREND):
+                    # enter into a position
+                    print("Buying Coin :" + coin)
                     print(datetime.now())
-                    print("Purchase Price: " + str(PURCHASE_PRICE))
-                    print("Exit Price:" + str(current_price))
+                    print("Price:" + str(current_price))
 
-                    sell_coin(coin)
+                    buy_coin(coin)
 
-                # stop loss
-                if config.STOPLOSS_PERCENT != 0 and PURCHASE_PRICE * ((100.0 - config.STOPLOSS_PERCENT) / 100.0) >= current_price: 
-                    print("Stoploss Triggered")
-                    print(datetime.now())
-                    print("Selling at: " + str(current_price))
-                    print("Purchased at:" + str(PURCHASE_PRICE))
-                    
-                    sell_coin(coin)
-                    COOLDOWN_SECONDS = config.COOLDOWN_SECONDS
+                if POSITION:
+                    # potential profit
+                    if sd.is_coin_overbought(coin , UPTREND):
+                        print("Exiting Position")
+                        print(datetime.now())
+                        print("Purchase Price: " + str(PURCHASE_PRICE))
+                        print("Exit Price:" + str(current_price))
 
-        time.sleep(10.0)
+                        sell_coin(coin)
+
+                    # stop loss
+                    if config.STOPLOSS_PERCENT != 0 and PURCHASE_PRICE * ((100.0 - config.STOPLOSS_PERCENT) / 100.0) >= closing_price: 
+                        try:
+                            file_object = open('log.txt', 'a')
+                            file_object.write("\n")
+                            file_object.write('Stoploss Triggered')
+                            file_object.write(str(datetime.now()))
+                            file_object.close()
+                        finally:
+                            print("Stoploss Triggered")
+                            print(datetime.now())
+                            print("Selling at: " + str(current_price))
+                            print("Purchased at:" + str(PURCHASE_PRICE))
+                        
+                        sell_coin(coin)
+                        COOLDOWN_SECONDS = config.COOLDOWN_SECONDS
+        except Exception as e:
+            print("Exception Occured")
+            print(str(e))
+
+        time.sleep(30.0)
 
 
 if __name__ == "__main__":
--- scout_data.py
+++ scout_data.py
@@ -2,39 +2,66 @@ import config
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 from datetime import datetime
 import numpy as np
-from talib import RSI,BBANDS
+from talib import RSI,BBANDS,SMA
 import time
 import csv
 
 
 client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
-def get_coin_price(coin):
+def get_latest_coin_price(coin):
     try:
         candles = client.get_klines(symbol=(coin + 'USD') , interval=Client.KLINE_INTERVAL_15MINUTE , limit=1)
         return float(candles[-1][4])
     except:
-        return float(-1)
+        return float(-1.0)
 
-def is_coin_oversold(coin):
+def get_latest_closing_price(coin):
+    try:
+        candles = client.get_klines(symbol=(coin + 'USD') , interval=Client.KLINE_INTERVAL_15MINUTE , limit=10)
+        return float(candles[-2][4])
+    except:
+        return float(-1.0)
+
+def get_coin_mov_avg(coin , period):
+        try:
+            candles = client.get_klines(symbol=(coin + 'USD') , interval=Client.KLINE_INTERVAL_15MINUTE , limit=period)
+            candles = candles[:-1]
+            closing_prices = np.array([float(x) for x in ([y[4] for y in candles])])
+            mov_avg = SMA(closing_prices , timeperiod=period-1)
+            return float(mov_avg[-1])
+        except Exception as e:
+            print(e)
+            return float(-1.0)
+
+
+def is_coin_oversold(coin , uptrend):
     candles = client.get_klines(symbol=(coin + 'USD') , interval=Client.KLINE_INTERVAL_15MINUTE , limit=150)
     candles = candles[:-1]
     closing_prices = np.array([float(x) for x in ([y[4] for y in candles])])
     rsi = RSI(closing_prices, timeperiod=config.RSI_TIME_PERIOD)[-1]
 
-    if (rsi <= config.RSI_OVERSOLD):
-        return True
+    if uptrend:
+        if (rsi <= config.RSI_OVERSOLD_UPTREND):
+            return True
+    else:
+        if (rsi <= config.RSI_OVERSOLD_DOWNTREND):
+            return True
 
     return False
 
-def is_coin_overbought(coin):
+def is_coin_overbought(coin , uptrend):
     candles = client.get_klines(symbol=(coin + 'USD') , interval=Client.KLINE_INTERVAL_15MINUTE , limit=150)
     candles = candles[:-1]
     closing_prices = np.array([float(x) for x in ([y[4] for y in candles])])
     rsi = RSI(closing_prices, timeperiod=config.RSI_TIME_PERIOD)[-1]
     
-    if (rsi >= config.RSI_OVERBOUGHT):
-        return True
+    if uptrend:
+        if (rsi >= config.RSI_OVERBOUGHT_UPTREND):
+            return True
+    else:
+        if (rsi >= config.RSI_OVERBOUGHT_DOWNTREND):
+            return True
 
     return False
 
@@ -42,7 +69,7 @@ def get_oversold_coins(coin_list):
     oversold_coins = []
 
     for coin in coin_list:
-        if is_coin_oversold(coin):
+        if is_coin_oversold(coin , True):
             oversold_coins.append(coin)
 
     return oversold_coins
@@ -50,17 +77,24 @@ def get_oversold_coins(coin_list):
 
 def main():
 
-    print(get_oversold_coins(config.supported_coins))
     LTCBALANCE = client.get_asset_balance(asset='LTC')['free']
     USDBALANCE = client.get_asset_balance(asset='USD')['free']
 
     print(LTCBALANCE)
     print(USDBALANCE)
 
-    # candles = client.get_historical_klines("LTCUSD", Client.KLINE_INTERVAL_15MINUTE, "1 Jan, 2020")
+    # candles  = client.get_historical_klines("LTCUSD", Client.KLINE_INTERVAL_15MINUTE, "1 Dec, 2019")
 
-    # print(len(candles))
-    # print(candles[-1])
+    # print(get_latest_coin_price('LTC'))
+    # print(get_latest_closing_price('LTC'))
+    print(get_coin_mov_avg('LTC' , 200))
+    print(is_coin_overbought('LTC' , True))
+    print(is_coin_overbought('LTC' , False))
+    print(is_coin_oversold('LTC' , True))
+    print(is_coin_oversold('LTC' , False))
+    # print(get_coin_mov_avg('LTC' , 100))
+    # print(get_coin_mov_avg('LTC' , 50))
+
     # csvfile = open('15minute_2020_LTC.csv' , 'w' , newline='')
     # candlestick_write = csv.writer(csvfile)
 
